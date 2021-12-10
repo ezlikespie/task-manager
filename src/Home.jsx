656d4af6f99924bb85c64d438010c2998f5b0c2d
BuildTools <ethan@zentz.net>
2021-12-10 00:21:10 -0500

    stuff

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,36 @@
 import React, {useState} from "react"
-import {Typography, Button, Fab, Card, TextField} from '@mui/material';
+import {Typography, Button, Fab, Card, TextField, Slider, ToggleButton, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 import {Add} from "@mui/icons-material";
 import { v4 as uuidv4 } from 'uuid';
 import Task from "./Task"
+import DatePicker from "react-datepicker"
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-const Home = () => {
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import "react-datepicker/dist/react-datepicker.css";
+
+const Home = ({themeCallback}) => {
 
     const [tasks, setTasks] = useState([])
-    const TASK_PROTOTYPE = {"Title":"","Description":""}
-    const TASK_PROPS = ["Title","Description","Priority"]
+    const TASK_PROTOTYPE = {"Title":"","Description":"","Class":"","Date":new Date(),"Priority":5}
+    const TASK_PROPS = ["Title","Description","Class","Date","Priority"]
+    const TEXTFIELD_PROPS = ["Title", "Description", "Class"]
+    const [showError, setShowError] = useState(false)
+    const [darkMode, setDarkMode] = useState(false)
+    const [sort, setSort] = useState(1)
     const [createData, setCreateData] = useState(TASK_PROTOTYPE)
 
     const addTask = () => {
-        createData["id"] = uuidv4()
-        setTasks([...tasks, createData])
-        setCreateData(TASK_PROTOTYPE)
+        let invalidProp = false;
+        TASK_PROPS.map((prop)=>{if(createData[prop]==null||createData[prop]==""){invalidProp = true;}})
+        if(invalidProp)
+            setShowError(true)
+        else {
+            createData["id"] = uuidv4()
+            setTasks([...tasks, createData])
+            setCreateData(TASK_PROTOTYPE)
+            setShowError(false)
+        }
     }
 
     const createObject = (key, prop) => {
@@ -40,10 +56,39 @@ const Home = () => {
         })
     }
 
+    const [myTheme, setMyTheme] = useState(createTheme({
+    palette: {
+        mode: 'light',
+    }
+    }))
+    const toggleTheme = () => {
+    setMyTheme(createTheme({
+        palette: {
+        mode: (darkMode)?'light':'dark',
+        }
+    }))
+    setDarkMode(!darkMode)
+    }
+
+    const sortTasks = (a,b) => {
+        if(sort==1)
+            return (a["Date"] > b["Date"])?1:-1
+        if(sort==2)
+            return (a["Date"] < b["Date"])?1:-1
+    }
+
     return (
+        <ThemeProvider theme={myTheme}>
         <div style={{maxWidth: "60%", margin: "auto", padding: "2em 1em"}}>
             <div style={{height: "fit-content"}}>
                 <Typography variant="h1" style={{fontWeight: "bold", textAlign: "center", fontSize: "2em"}}>Awesome Task App</Typography>
+                {/*<ToggleButton
+                    value="check"
+                    selected={darkMode}
+                    onChange={toggleTheme}
+                    >
+                    <DarkModeIcon />
+                </ToggleButton>*/}
                 <Card 
                     style={{
                         width: "100%", 
@@ -52,18 +97,51 @@ const Home = () => {
                         padding: "1em"
                     }} 
                 >
-                {Object.keys(TASK_PROTOTYPE).map((taskKey) => (
+                {TEXTFIELD_PROPS.map((taskKey) => (
                     <TextField style={{marginTop: "8px"}} fullWidth label={taskKey} variant="outlined" value={createData[taskKey]} onChange={(e) => setCreateData({...createData, ...createObject(taskKey, e.target.value) }) } />
                 ))}
-                <Button style={{marginTop: "8px"}}onClick={addTask} >Add Task</Button>
+
+                <DatePicker style={{display: "block", marginTop: "8px"}} selected={createData["Date"]} onChange={(date) => {setCreateData({...createData, ...createObject("Date", date)})}} />
+                <Typography variant="p">Priority:</Typography>
+                <Slider
+                    aria-label="Priority"
+                    defaultValue={createData["Priority"]}
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    onChange={(e) => {setCreateData({...createData, ...createObject("Priority", e.target.value)})}}
+                    min={1}
+                    max={10}
+                    />
+                <div>
+                <Button style={{ marginTop: "8px"}} onClick={addTask}>Add Task</Button>
+                <Typography variant="p" style={{display: (showError)?"block":"none", marginLeft: "1em"}}>One or more fields are incorrect</Typography>
+                </div>
                 </Card>
+                <FormControl>
+                    <InputLabel id="demo-simple-select-label">Sort</InputLabel>
+                    <Select
+                        value={sort}
+                        label="Sort"
+                        onChange={(e)=>setSort(e.target.value)}
+                    >
+                        <MenuItem value={1}>Date Ascending</MenuItem>
+                        <MenuItem value={2}>Date Descending</MenuItem>
+                        <MenuItem value={3}>Class</MenuItem>
+                        <MenuItem value={4}>Priority</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
+                
             </div>
             <div style={{marginTop: "10px"}}>
-                {tasks.map((task) => (
+                {tasks.sort(sortTasks).map((task) => (
                     <Task task={task} callbackDelete={removeTask} callbackEdit={editTask} uuid={task["id"]} />
                 ))}
             </div>
         </div>
+        </ThemeProvider>
     )
 }
 
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from "react"
-import {Typography, Card, Button, TextField} from "@mui/material";
+import {Typography, Card, Button, TextField, Slider} from "@mui/material"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css";
 
 const Task = ({task, callbackDelete, callbackEdit}) => {
 
     const [raised, setRaised] = useState(false)
     const [editMode, setEditMode] = useState(false)
     const [editData, setEditData] = useState(task)
+    const [startDate, setStartDate] = useState(task["Date"])
 
     const createObject = (key, prop) => {
         var temp = new Object()
@@ -18,6 +21,18 @@ const Task = ({task, callbackDelete, callbackEdit}) => {
         setEditMode(!editMode)
     }
 
+    function getFormattedDate(date) {
+        var year = date.getFullYear();
+        
+        var month = (1 + date.getMonth()).toString();
+        month = month.length > 1 ? month : '0' + month;
+        
+        var day = date.getDate().toString();
+        day = day.length > 1 ? day : '0' + day;
+        
+        return month + '/' + day + '/' + year;
+    }
+
     return (
         <Card 
             raised={raised} 
@@ -26,7 +41,8 @@ const Task = ({task, callbackDelete, callbackEdit}) => {
                 minHeight: "200px",
                 margin: "1em",
                 padding: "1em",
-                position: "relative"
+                position: "relative",
+                overflow: "visible"
             }} 
             onMouseOver={() => setRaised(true)} 
             onMouseLeave={() => setRaised(false)}
@@ -34,16 +50,33 @@ const Task = ({task, callbackDelete, callbackEdit}) => {
             <div style={{
                 display: (editMode)?"none":"initial"
             }}>
-            <Typography variant="h6">{task["Title"]}</Typography>
-            <Typography>{task["Description"]}</Typography>
+            <Typography variant="h6">Title: {task["Title"]}</Typography>
+            <Typography>Description: {task["Description"]}</Typography>
+            <Typography>Class: {task["Class"]}</Typography>
+            <Typography>Date: {getFormattedDate(task["Date"])}</Typography>
+            <Typography>Priority: {task["Priority"]}</Typography>
             </div>
             <div style={{
                 display: (editMode)?"initial":"none"
             }}>
             <TextField label="Title" defaultValue={task["Title"]} fullWidth variant="outlined" onChange={(e) => setEditData({...editData, ...createObject("Title", e.target.value) }) }/>
-            <TextField label="Description" defaultValue={task["Description"]} fullWidth variant="outlined" onChange={(e) => setEditData({...editData, ...createObject("Description", e.target.value) }) }/>
+            <TextField label="Description" defaultValue={task["Description"]} style={{marginTop: "8px"}} fullWidth variant="outlined" onChange={(e) => setEditData({...editData, ...createObject("Description", e.target.value) }) }/>
+            <TextField label="Class" defaultValue={task["Class"]} style={{marginTop: "8px"}} fullWidth variant="outlined" onChange={(e) => setEditData({...editData, ...createObject("Class", e.target.value) }) }/>
+            <DatePicker style={{display: "block", marginTop: "8px"}} selected={startDate} onChange={(date) => {setStartDate(date); setEditData({...editData, ...createObject("Date", date)})}} />
+            <Typography style={{marginTop: "8px"}} variant="p">Priority:</Typography>
+            <Slider
+                style={{marginTop: "8px", display:"block", zIndex: 10 }}
+                aria-label="Priority"
+                defaultValue={task["Priority"]}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={(e) => setEditData({...editData, ...createObject("Priority", e.target.value)})}
+                />
             </div>
-            <Button onClick={() => callbackDelete(task["id"])} style={{display:(editMode)?"none":"initial"}}>Delete Task</Button>
+            <Button onClick={() => callbackDelete(task["id"])} style={{display:(editMode)?"none":"initial"}}>Mark Complete</Button>
             <Button onClick={editTask}>{(editMode)?"Save Task":"Edit Task"}</Button>
         </Card>
     )
